24469276e74b76020d5860ada0d402679652c305
Shohag Faraji <shohagfaraji2@gmail.com>
2026-01-23 03:17:14 +0600

    Display products in frontend with Tailwind CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,38 @@
 import { useEffect, useState } from "react";
-import "./App.css";
+// import "./App.css";
 
 function App() {
-    const [message, setMessage] = useState("");
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        fetch("http://127.0.0.1:8000/api")
+        fetch("http://127.0.0.1:8000/api/products/")
             .then((response) => response.json())
-            .then((data) => setMessage(data.message))
-            .catch((error) => console.error("Error fetching message:", error));
+            .then((data) => setProducts(data))
+            .catch((error) => console.error(error));
     }, []);
 
     return (
         <>
-            <div>
-                <h1>Message from backend</h1>
-                <p>{message || "Loading..."}</p>
+            <div className="min-h-screen bg-gray-100 test-gray-800">
+                <h1 className="text-3xl font-bold">Product list</h1>
+                <div className="container mx-auto p-4">
+                    {products.map((product) => (
+                        <div
+                            key={product.id}
+                            className="bg-white p-4 rounded shadow mb-4"
+                        >
+                            <h2 className="text-xl font-semibold">
+                                {product.name}
+                            </h2>
+                            <p className="text-gray-600">
+                                {product.description}
+                            </p>
+                            <p className="text-gray-600 font-bold">
+                                {product.price} BDT
+                            </p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
     );
