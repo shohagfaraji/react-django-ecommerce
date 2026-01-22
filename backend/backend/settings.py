3d6ab3cb97c8cf5cae86bbde066bf20185766152
Shohag Faraji <shohagfaraji2@gmail.com>
2026-01-22 22:13:58 +0600

    Connect React frontend to Django API

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = [
+    "http://localhost:3000",
+]
--- backend/store/views.py
+++ backend/store/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def home(request):
+    data = {
+        'message' : 'Welcome to the E-commerce Store!'
+    }
+    return JsonResponse(data)
