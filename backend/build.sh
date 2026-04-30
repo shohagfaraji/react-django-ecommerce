878cdbe63a224c65a68fae60c29d6a666ae5d434
Shohag Faraji <shohagfaraji2@gmail.com>
2026-05-01 00:40:14 +0600

    fix: upgrade Django to 5.1.7 for Python 3.14 compatibility

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
+pip install --upgrade pip
+pip install "Django==5.1.7"
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
